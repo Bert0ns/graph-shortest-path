c7862fc4b59b49985b0feb141b4050ca9dcc4a22
Davide Bertoni <121551954+Bert0ns@users.noreply.github.com>
2025-10-29 00:26:40 +0100

    ci: preparing for themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,37 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import React from "react";
 import {Toaster} from "@/components/ui/sonner";
 import ThemeProvider from "@/components/theme-components/themeProvider";
 import WebsiteHeader from "@/components/websiteHeader";
 import {websiteConfigs} from "@/website.configs";
-import {NextFontWithVariable} from "next/dist/compiled/@next/font";
-import {Poppins} from "next/font/google";
 
-
-const poppins: NextFontWithVariable = Poppins({
-    subsets: ['latin'],
-    weight: ['400', '600', '700'],
-    variable: '--font-poppins',
-    display: 'swap',
-})
 
 export const metadata: Metadata = {
-  title: websiteConfigs.title,
-  description: websiteConfigs.description,
+    title: websiteConfigs.title,
+    description: websiteConfigs.description,
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${poppins.variable} font-serif antialiased min-h-screen`}>
+    return (
+        <html lang="en">
+        <body className={`font-serif antialiased min-h-screen`}>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
+            themes={['system', 'light', 'dark', 'blue']}
         >
-            <WebsiteHeader />
+            <WebsiteHeader/>
             {children}
-            <Toaster position="bottom-left" />
+            <Toaster position="bottom-left"/>
         </ThemeProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
