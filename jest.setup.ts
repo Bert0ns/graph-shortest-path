37637ad4f00aca3c9ffe7afdd5d861b4aabfb434
Davide Bertoni <121551954+Bert0ns@users.noreply.github.com>
2025-10-30 00:46:09 +0100

    fix: ovverriding act function in order to prevent error

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom'
+import { act } from 'react';
+
+//To solve act() warning in tests (sub dependencies using react-dom/test-utils act not updated)
+// Create mock module 'react-dom/test-utils'
+jest.mock('react-dom/test-utils', () => {
+    // keep all origina exports
+    const original = jest.requireActual('react-dom/test-utils');
+
+    return {
+        ...original,
+        // overwrite 'act' function
+        act: (callback: () => unknown) => {
+            return act(callback);
+        },
+    };
+});
